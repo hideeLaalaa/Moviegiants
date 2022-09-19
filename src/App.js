c99ed2c36374de1d94c384a481b312e82051708c
Adesina Idris <50444175+hideeLaalaa@users.noreply.github.com>
2022-09-19 15:21:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
   }, [])
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`,{ mode: 'no-cors'})
+    console.log(response);
     const data = await response.json()
     console.log(data);
     setMovies(data.Search)
