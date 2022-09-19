e885bc43fe4bb349cffb9d4b76b3852358b4c798
Adesina Idris <50444175+hideeLaalaa@users.noreply.github.com>
2022-09-19 14:37:09 +0100

    https trial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard'
 import './App.css'
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=9b4cef1d'
+const API_URL = 'https://www.omdbapi.com?apikey=9b4cef1d'
 
 // const movie1 = {
 //   Title: 'Superman, Spiderman or Batman',
